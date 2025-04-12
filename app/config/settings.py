6fc3f0adc1b466deb97f56f66742e85fe1f5b3cf
Kristofer Borges <kristofer.souza2027@hotmail.com>
2025-04-12 14:19:21 -0300

    Ajustes na geração do arquivo de exportação

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,7 +6,7 @@ class Settings:
         self.skip = False
         self.caminhoGameData = "config/gameData.yaml"
         self.aumentoDePoder = 410
-        self.quantidadeDeJogos = 10
+        self.quantidadeDeJogos = 100
         self.velocidadeDoJogo = 0.0
         self.velocidadeDoJogoBoss = 0.0
         self.danoAoJogador = 30
